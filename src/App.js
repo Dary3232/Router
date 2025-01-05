ae9f422e2757ecb18b70ce01f54213bdef204120
Darya Shkuratova <d.legascheva@gmail.com>
2025-01-05 21:56:52 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Posts } from "./pages/posts";
+import { Root } from "./components/Root";
+import { Main } from "./pages/main";
+import { Blog } from "./pages/blog";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          index: true,
+          element: <Main />,
+        },
+        {
+          path: "posts",
+          element: <Posts />,
+        },
+        {
+          path: "blog",
+          element: <Blog />
+        }
+      ],
+    },
+  ]);
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Root/index.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
+import styles from "./style.css"
+
+<NavLink
+    className={({ isActive }) => {
+        return isActive ? styles.active : "";
+    }}
+/>
+
+export const Root = () => <>
+    <NavLink to='/'>Главная</NavLink>
+    <NavLink to='posts'>Посты</NavLink>
+    <NavLink to='blog'>Блог</NavLink>
+    <br />
+    <br />
+    <br />
+    <br />
+    <Outlet />
+</>
+
+
+// export const Root = () => (
+//   <>
+//     <NavLink
+//       to='/'
+//       className={({ isActive }) => (isActive ? styles.active : "")}
+//     >
+//       Главная
+//     </NavLink>
+//     <NavLink
+//       to='posts'
+//       className={({ isActive }) => (isActive ?  styles.active : "")}
+//     >
+//       Посты
+//     </NavLink>
+//     <NavLink
+//       to='blog'
+//       className={({ isActive }) => (isActive ? styles.active : "")}
+//     >
+//       Блог
+//     </NavLink>
+//     <br />
+//     <br />
+//     <br />
+//     <br />
+//     <Outlet />
+//   </>
+// );
+
